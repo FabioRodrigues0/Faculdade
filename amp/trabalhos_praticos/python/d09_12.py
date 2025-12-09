eea4aeb0dd6e0b577287fed36df5485dd6e4cdc1
FabioRodrigues0 <fabio.rod@outlook.pt>
2025-12-09 15:11:50 +0000

    refractor: apagado import feito por engano do ide

--- amp/trabalhos_praticos/python/d09_12.py
+++ amp/trabalhos_praticos/python/d09_12.py
@@ -1,8 +1,6 @@
 # Lista de exercícios deste dia
-from string import printable
-
-
 exercicios = {}
+
 
 def exercicio_1():
     """
@@ -11,27 +9,29 @@ def exercicio_1():
     list_num = {i: i**2 for i in range(1, 17)}
     print(list_num)
 
+
 def exercicio_2():
     """
     Escrever o numero de dias de um mes a partir do nome do mes.
     """
     meses = {
-        'Janeiro': 31,
-        'Fevereiro': 28,
-        'Março': 31,
-        'Abril': 30,
-        'Maio': 31,
-        'Junho': 30,
-        'Julho': 31,
-        'Agosto': 31,
-        'Setembro': 30,
-        'Outubro': 31,
-        'Novembro': 30,
-        'Dezembro': 31
+        "Janeiro": 31,
+        "Fevereiro": 28,
+        "Março": 31,
+        "Abril": 30,
+        "Maio": 31,
+        "Junho": 30,
+        "Julho": 31,
+        "Agosto": 31,
+        "Setembro": 30,
+        "Outubro": 31,
+        "Novembro": 30,
+        "Dezembro": 31,
     }
     mes = input("Digite o nome do mês: ").capitalize()
-    
+
     print(f"{mes} tem {meses.get(mes, 'Mês inválido')} dias.")
+
 
 def exercicio_3():
     """
@@ -40,10 +40,11 @@ def exercicio_3():
         list1: keys
         list2: values
     """
-    lista1 = ['a', 'b', 'c']
+    lista1 = ["a", "b", "c"]
     lista2 = [1, 2, 3]
-    
+
     print(dict(zip(lista1, lista2)))
+
 
 def exercicio_4():
     """
@@ -52,12 +53,13 @@ def exercicio_4():
         dicionario - vogal: numero_de_ocorrencias
     """
     frase = input("Digite uma frase: ").lower()
-    vogais = 'aeiou'
+    vogais = "aeiou"
     contador = {vogal: frase.count(vogal) for vogal in vogais}
-    
+
     print(contador)
 
-exercicios['exercicio_1'] = exercicio_1
-exercicios['exercicio_2'] = exercicio_2
-exercicios['exercicio_3'] = exercicio_3
-exercicios['exercicio_4'] = exercicio_4
+
+exercicios["exercicio_1"] = exercicio_1
+exercicios["exercicio_2"] = exercicio_2
+exercicios["exercicio_3"] = exercicio_3
+exercicios["exercicio_4"] = exercicio_4
